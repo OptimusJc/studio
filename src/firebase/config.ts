1108672cd4944d8f151764eca41d7f3f4e57f375
Joe Cruz <Optimus.Prime.JC@gmail.com>
2025-10-15 10:30:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-56609462-aefda",
+  "appId": "1:812626060223:web:934f52aa92e41c80dd370a",
+  "apiKey": "AIzaSyCElvD2uLg_2q4Bf68XhJ8M6RoG_cco-RM",
+  "authDomain": "studio-56609462-aefda.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "812626060223"
+};
