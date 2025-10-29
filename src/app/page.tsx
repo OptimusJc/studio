ead36c49631a173c7267c82e120ca7e8d969bb44
Joe Cruz <Optimus.Prime.JC@gmail.com>
2025-10-29 20:38:59 +0000

    Fix the placeholder-data.ts examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 
 export default function Home() {
+  console.log("Hey I'm here!!")
   // redirect('/admin')
   return (
     <h1>This is the main page</h1>
