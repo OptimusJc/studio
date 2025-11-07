b9b2c3fe7da93a854127a411eee6f3f0d6a91f6f
Joe Cruz <Optimus.Prime.JC@gmail.com>
2025-11-07 16:25:30 +0000

    give them the size of 14

--- src/app/shop/components/Header.tsx
+++ src/app/shop/components/Header.tsx
@@ -100,8 +100,8 @@ export default function Header({ categories, appliedFilters, onFilterChange, sea
         <div className="lg:hidden flex items-center gap-1">
              <Dialog open={mobileSearchOpen} onOpenChange={setMobileSearchOpen}>
                 <DialogTrigger asChild>
-                    <Button variant="ghost" className="p-2">
-                        <Search className='h-8 w-8' />
+                    <Button className="p-2">
+                        <Search className='h-14 w-14' />
                     </Button>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-md top-[25%]">
@@ -119,15 +119,15 @@ export default function Header({ categories, appliedFilters, onFilterChange, sea
                 </DialogContent>
              </Dialog>
 
-             <Button variant="ghost" className="p-2" onClick={openMobileFilters}>
-                <Filter className="h-8 w-8" />
+             <Button className="p-2" onClick={openMobileFilters}>
+                <Filter className="h-14 w-14" />
              </Button>
             
             {hasNav && (
                  <Sheet>
                     <SheetTrigger asChild>
-                        <Button variant="ghost" className="p-2">
-                            <Menu className="h-8 w-8" />
+                        <Button className="p-2">
+                            <Menu className="h-14 w-14" />
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="right" className="w-full max-w-xs">
