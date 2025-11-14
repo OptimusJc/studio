6280ea9543df3d8fa02a3a6e6c60cf05acc66481
OptimusJc <optimus.prime.jc@gmail.com>
2025-11-14 14:49:34 +0300

    update logo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,11 +1,10 @@
-
-import Image from 'next/image';
-import { cn } from '@/lib/utils';
+import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/studio-56609462-aefda.appspot.com/o/assets%2Flogo.png?alt=media&token=8d249539-5586-4531-b856-787c9343e0e7"
+      src="/icon.svg"
       alt="Ruby Catalogue Logo"
       width={140}
       height={32}
