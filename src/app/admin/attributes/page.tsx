4694de6d3494e7f84502c01b0ca279ff882e75c9
Joe Cruz <Optimus.Prime.JC@gmail.com>
2025-12-22 07:13:05 +0000

    Make the attributes list from the admin panel scrollable when the list o

--- src/app/admin/attributes/page.tsx
+++ src/app/admin/attributes/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useMemoFirebase, useCollection, useFirestore, deleteDocumentNonBlocking } from '@/firebase';
@@ -11,6 +12,7 @@ import { Badge } from '@/components/ui/badge';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { AddAttributeDialog } from './components/AddAttributeDialog';
 import { EditAttributeDialog } from './components/EditAttributeDialog';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 type Attribute = {
   id: string;
@@ -50,57 +52,59 @@ export default function AttributesPage() {
           <CardDescription>A list of all product attributes and their possible values.</CardDescription>
         </CardHeader>
         <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Attribute Name</TableHead>
-                <TableHead>Category</TableHead>
-                <TableHead>Values</TableHead>
-                <TableHead className="w-[40px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {isLoading && (
+          <ScrollArea className="max-h-[60vh]">
+            <Table>
+              <TableHeader>
                 <TableRow>
-                  <TableCell colSpan={4} className="text-center">Loading attributes...</TableCell>
+                  <TableHead>Attribute Name</TableHead>
+                  <TableHead>Category</TableHead>
+                  <TableHead>Values</TableHead>
+                  <TableHead className="w-[40px]"></TableHead>
                 </TableRow>
-              )}
-              {!isLoading && attributes?.map((attribute) => (
-                <TableRow key={attribute.id}>
-                  <TableCell className="font-medium">{attribute.name}</TableCell>
-                  <TableCell>
-                    <Badge variant="outline">{attribute.category}</Badge>
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex flex-wrap gap-2">
-                      {attribute.values.map((value) => (
-                        <Badge key={value} variant="secondary">{value}</Badge>
-                      ))}
-                    </div>
-                  </TableCell>
-                  <TableCell>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <EditAttributeDialog attribute={attribute} />
-                        <DropdownMenuItem
-                          className="text-destructive"
-                          onClick={() => handleDelete(attribute.id)}
-                        >
-                          Delete
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody>
+                {isLoading && (
+                  <TableRow>
+                    <TableCell colSpan={4} className="text-center">Loading attributes...</TableCell>
+                  </TableRow>
+                )}
+                {!isLoading && attributes?.map((attribute) => (
+                  <TableRow key={attribute.id}>
+                    <TableCell className="font-medium">{attribute.name}</TableCell>
+                    <TableCell>
+                      <Badge variant="outline">{attribute.category}</Badge>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex flex-wrap gap-2">
+                        {attribute.values.map((value) => (
+                          <Badge key={value} variant="secondary">{value}</Badge>
+                        ))}
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <EditAttributeDialog attribute={attribute} />
+                          <DropdownMenuItem
+                            className="text-destructive"
+                            onClick={() => handleDelete(attribute.id)}
+                          >
+                            Delete
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </ScrollArea>
         </CardContent>
       </Card>
     </div>
