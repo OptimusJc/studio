8e8854a575c5e513dabe112b512b84b4fa04af9d
OptimusJc <optimus.prime.jc@gmail.com>
2026-01-08 10:49:35 +0300

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
-
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
-import { FirebaseClientProvider } from '@/firebase/client-provider';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Inter, Source_Code_Pro, Playfair_Display } from 'next/font/google';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { FirebaseClientProvider } from "@/firebase/client-provider";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter, Source_Code_Pro, Playfair_Display } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
 });
 
 const sourceCodePro = Source_Code_Pro({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-source-code-pro',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-source-code-pro",
 });
 
 const playfairDisplay = Playfair_Display({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-playfair-display',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-playfair-display",
 });
 
-
 export const metadata: Metadata = {
-  title: 'Ruby Catalogue',
-  description: 'Manage and share your product catalogs with ease.',
+  title: "Ruby Catalogue",
+  description: "Manage and share your product catalogs with ease.",
 };
 
 export default function RootLayout({
@@ -36,21 +35,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={`${inter.variable} ${sourceCodePro.variable} ${playfairDisplay.variable}`}>
-      <head>
-      </head>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={`${inter.variable} ${sourceCodePro.variable} ${playfairDisplay.variable}`}
+    >
+      <head></head>
       <body className="font-body antialiased">
         <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
         >
-            <FirebaseClientProvider>
-            {children}
-            </FirebaseClientProvider>
-            <Toaster />
+          <FirebaseClientProvider>{children}</FirebaseClientProvider>
+          <Toaster />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
